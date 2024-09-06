84b63bef04e9376697545e54cec1fdde10c3caeb
MehmetDenizUckan <deniz.uckan@gmail.com>
2024-09-06 11:19:56 +0300

    changed config

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ class Config:
     # Default to the DATABASE_URL from the environment (used in production/server)
     DATABASE_URL = os.getenv('DATABASE_URL', LOCAL_DATABASE_URL)
 
+    # Append sslmode=require for Heroku's PostgreSQL connection
+    if DATABASE_URL.startswith('postgres://'):
+        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)  # Ensure correct URI scheme
+        DATABASE_URL += '?sslmode=require'
+
     # Flask environment (development or production)
     FLASK_ENV = os.getenv('FLASK_ENV', 'production')
-
