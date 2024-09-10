79dbf422870e2c1d6cd6edb1ab4f3014e006334e
MehmetDenizUckan <deniz.uckan@gmail.com>
2024-09-10 12:51:26 +0300

    server db fix

--- create_server_db.py
+++ create_server_db.py
@@ -7,11 +7,14 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-# Create a table
+# Drop the table if it exists
+cur.execute("DROP TABLE IF EXISTS users;")
+
+# Create the table
 cur.execute("""
-    CREATE TABLE users (
+    CREATE TABLE user_info (
         id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
+        user_name VARCHAR(50) UNIQUE NOT NULL,
         user_email VARCHAR(100) UNIQUE NOT NULL,
         user_password TEXT NOT NULL,
         user_comments VARCHAR(100),
